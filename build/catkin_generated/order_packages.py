af6e349d3bf4171a7eb6ffbf71fd853fff64bbdc
Jhonn <105321843+JhonnNinaja@users.noreply.github.com>
2024-02-27 17:23:23 -0400

    Add files via upload

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/jhonn/Documents/robo/kuka_ws/src'
+whitelisted_packages = 'tutorial'.split(';') if 'tutorial' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/jhonn/Documents/robo/kuka_ws/devel;/opt/ros/noetic'.split(';') if '/home/jhonn/Documents/robo/kuka_ws/devel;/opt/ros/noetic' != '' else []
